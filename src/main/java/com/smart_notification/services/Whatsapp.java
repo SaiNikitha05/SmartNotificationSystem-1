8888355d7f7e9a39d1206a0327e35b7ce75c6a14
SaiNikitha <nikitha.neerukonda24@gmail.com>
2025-06-28 16:22:02 +0530

    Update Whatsapp.java

--- src/main/java/com/smart_notification/services/Whatsapp.java
+++ src/main/java/com/smart_notification/services/Whatsapp.java
@@ -4,6 +4,6 @@ public class Whatsapp {
 
     public void send_notification(String name, long number) {
         System.out.println(
-                "Hey " + name + ", Thanks for checking out my project.Since you prefer whatsapp, here we are!");
+                "Hey " + name + ", Thanks for checking out my project.Since you prefer whatsapp, here we are!");  //the actual api will be called here
     }
 }
