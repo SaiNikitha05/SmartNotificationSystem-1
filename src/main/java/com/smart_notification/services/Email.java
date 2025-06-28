bf0111da0a60fadfc07fdfdb49f6e811630b4be9
SaiNikitha <nikitha.neerukonda24@gmail.com>
2025-06-28 16:22:38 +0530

    Update Email.java

--- src/main/java/com/smart_notification/services/Email.java
+++ src/main/java/com/smart_notification/services/Email.java
@@ -2,6 +2,6 @@ package com.smart_notification.services;
 
 public class Email {
     public void send_mail(String name, String info) {
-        System.out.println("Hey " + name + ", Thanks for checking out my project.Since you prefer email, here we are!");
+        System.out.println("Hey " + name + ", Thanks for checking out my project.Since you prefer email, here we are!");   //the actual api will be called here
     }
 }
